676756f21943da7819175e46c8b3c2fc8771b582
ohadpp <71223344+ohadpp@users.noreply.github.com>
2023-11-14 22:05:45 +0200

    Update news-headlines.py

--- news-headlines.py
+++ news-headlines.py
@@ -16,7 +16,6 @@ month_day_year = now.strftime("%d%m%y")  # DDMMYYYY
 website = "https://www.thesun.co.uk/"
 path = '/usr/local/bin/chromedriver'
 
-# Set up Chrome options
 chrome_options = Options()
 chrome_options._ignore_local_proxy = True
 
@@ -28,37 +27,29 @@ driver.get(website)
 
 containers = driver.find_elements(by='xpath', value="//div[@class='teaser__copy-container']")
 
-# List to store dictionaries for each container
 container_data_list = []
-
 for container in containers:
     try:
-        # Find the title within the current container
         title_element = container.find_element(by="xpath", value=".//span[@class='teaser__headline t-p-color']")
         title = title_element.text
 
-        # Find the subtitle within the current container
         subtitle_element = container.find_element(by="xpath", value="./a/p")
         subtitle = subtitle_element.text
 
-        # Find the link within the current container
         link_element = container.find_element(by="xpath", value="./a")
         link = link_element.get_attribute("href")
 
-        # Create a dictionary for the current container
         container_data = {
             "Title": title,
             "Subtitle": subtitle,
             "Link": link
         }
 
-        # Append the dictionary to the list
         container_data_list.append(container_data)
     except NoSuchElementException as e:
         # Handle the case where an element is not found
         print(f"Skipping container due to NoSuchElementException: {e}")
 
-# Print the list of dictionaries
 for container_data in container_data_list:
     print(container_data)
     print("\n")
